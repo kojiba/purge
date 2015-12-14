a026ef285975df2e81da4e0080c2457801f60df3
kojiba <kojiba@ro.ru>
2015-12-14 10:58:25 +0200

    added #define PURGE_DECRYPT_SPEEDUP // cause more memory usage, and attack on buffer with round keys

--- main.c
+++ main.c
@@ -198,12 +198,61 @@ void simpleCollisionTest() {
     }
 }
 
+void optimisationTest(){
+    byte array[purgeBytesCount] = {};
+    byte key[purgeBytesCount] = {};
+
+
+    printf("\n Optimisation Test --------------------------------\n");
+    printByteArrayInHex(array, purgeBytesCount);
+
+    byte cipherText[purgeBytesCount] = {};
+
+    initRClock();
+    purgeEncrypt((uint64_t *) array, (uint64_t *) key);
+    tickRClock();
+    printByteArrayInHex(array, purgeBytesCount);
+    memset(key, 0, purgeBytesCount);
+
+    tickRClock();
+    purgeDecrypt((uint64_t *) array, (uint64_t *) key);
+    tickRClock();
+    printByteArrayInHex(array, purgeBytesCount);
+
+    /* Etalon
+     *
+     * Encr Elapsed: 0.000053 seconds
+       Decr Elapsed: 0.000085 seconds
+
+     * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       B8 FF E8 11 EB 46 0F FE 12 AE 7F 34 E7 7A 03 49 18 B8 F8 AA EA F4 D6 3D 1F A8 98 35 35 C7 5C 42
+       91 42 7E 4C CF EA E4 30 56 6E 4B 28 19 4D D0 FA 72 55 FE DB 48 D5 79 FA 5A 1D 9B 47 10 9D E1 7E
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     */
+
+    /* PURGE_DECRYPT_SPEEDUP
+     *
+     * Encr Elapsed: 0.000060 seconds
+       Decr Elapsed: 0.000061 seconds
+
+     * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       B8 FF E8 11 EB 46 0F FE 12 AE 7F 34 E7 7A 03 49 18 B8 F8 AA EA F4 D6 3D 1F A8 98 35 35 C7 5C 42
+       91 42 7E 4C CF EA E4 30 56 6E 4B 28 19 4D D0 FA 72 55 FE DB 48 D5 79 FA 5A 1D 9B 47 10 9D E1 7E
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+       00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+     */
+}
+
 int main(int argc, const char *argv[]) {
 //
     cipherTestSimple();
     cipherTest();
     hashTest();
     simpleCollisionTest();
+    optimisationTest();
 
     exit(0);
 }
